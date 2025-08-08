cb60d8de5a195290dd44c202dfa7df0241a938da
Pranav Sahu <105783037+pranavsahu005@users.noreply.github.com>
2025-08-08 12:56:11 +0530

    urls.py

--- loginproject/loginproject/urls.py
+++ loginproject/loginproject/urls.py
@@ -1,17 +1,13 @@
 
 from django.contrib import admin
 from django.urls import path
-from django.conf import settings
-from . view import *
-from loginproject import view
+from . import view
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',view.home, name='home'),
-    path('front/',view.home, name='home'),
-    path('signin/',view.signin, name='signin'),
-    path('signup/',view.signup, name='signup'),
+    path('', view.home, name='home'),
+    path('front/', view.home, name='home'),
+    path('signin/', view.signin, name='signin'),
+    path('signup/', view.signup, name='signup'),
+    path('dashboard/', view.dashboard, name='dashboard'),
 ]
-
-
-
-
